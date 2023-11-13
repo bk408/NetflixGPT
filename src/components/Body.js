ac4a243dd2166a20e8b773df8caad56fd1251a41
Bhavya Khatri <bhavyakhatri1912@gmail.com>
2023-11-13 11:02:55 +0530

    browse com created/navigate to browse page/update user profile/sign out feature

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './Login'
 import Browse from './Browse'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../utils/firebase';
+import { useDispatch } from 'react-redux';
+import { addUser, removeUser } from '../utils/userSlice';
 
 const Body = () => {
+  const dispatch = useDispatch();
+  
 
   const appRouter = createBrowserRouter([
     {
@@ -15,6 +21,25 @@ const Body = () => {
       element: <Browse />
     }
   ])
+
+
+  useEffect(() => {
+
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed out
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(addUser({ uid: uid, email: email, displayName: displayName, photoURL: photoURL }));
+        // Redirect the user to browse page
+        
+      } else {
+        // User is signed out
+        dispatch(removeUser())
+        
+      }
+    });
+
+  }, [])
 
   return (
     <div>
